42bfe3804c4e751bdaf47d0dd9779588cadc6e84
Suman Adhikari <expertxkid@gmail.com>
2025-04-06 04:57:55 -0500

    feat/01-react-dev::03-thinking-in-react: complete project

--- 01-react-dev/03-thinking-in-react/src/FilterableProductTable.jsx
+++ 01-react-dev/03-thinking-in-react/src/FilterableProductTable.jsx
@@ -1,11 +1,22 @@
 import { SearchBar } from "./SearchBar";
 import { ProductTable } from "./ProductTable";
+import { useState } from "react";
 
 export function FilterableProductTable({products}){
+    const [filterText, setFilterText] = useState('fruit');
+    const [inStockOnly, setInStockOnly] = useState(false);
+    
     return (
     <div>
-        <SearchBar />
-        <ProductTable products={products} />
+        <SearchBar 
+            filterText={filterText} 
+            inStockOnly={inStockOnly}
+            onFilterTextChange={setFilterText}
+            onInStockOnlyChange={setInStockOnly} />
+        <ProductTable 
+            products={products} 
+            filterText={filterText} 
+            inStockOnly={inStockOnly} />
     </div>
 );
 }
