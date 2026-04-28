765917b30accc8141faa48406b509b1266a93ee3
Suman Adhikari <expertxkid@gmail.com>
2026-04-27 22:44:30 -0500

    feat(03-react-essentials): add component props

--- 03-tutorial-react-the-complete-guide-udemy/03-react-essentials/05-making-components-reusable-with-props/src/App.jsx
+++ 03-tutorial-react-the-complete-guide-udemy/03-react-essentials/05-making-components-reusable-with-props/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -21,13 +22,36 @@ function Header() {
   );
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <>
       <Header />
       <div>
         <main>
-          <h2>Time to get started!</h2>
+          <section id="core-concepts">
+            <h2>Time to get started!</h2>
+
+            <ul>
+              <CoreConcepts
+                title="Components"
+                description="The core UI building block."
+                img={componentsImg}
+              />
+              <CoreConcepts title="Props" />
+              <CoreConcepts />
+              <CoreConcepts />
+            </ul>
+          </section>
         </main>
       </div>
     </>
