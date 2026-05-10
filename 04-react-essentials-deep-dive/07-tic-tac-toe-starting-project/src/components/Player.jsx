81c77c5ab51ae2cadb6db130756fe174f264f131
Suman Adhikari <expertxkid@gmail.com>
2026-05-09 20:42:19 -0500

    feat(04-react-essentials-deep-dive): implement edit button

--- 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => setIsEditing((prev) => !prev);
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input value={name} required />;
+  }
+
   return (
     <li>
-      <span className="players">
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {playerName}
         <span className="plyaer-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
