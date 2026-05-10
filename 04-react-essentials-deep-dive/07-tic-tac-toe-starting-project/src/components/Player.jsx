aecfb4dc077b1db0691c56be64f6246796aa2679
Suman Adhikari <expertxkid@gmail.com>
2026-05-09 21:16:47 -0500

    refactor(04-react-essentials-deep-dive): add two-way databinding for playerName

--- 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [editablePlayerName, setPlayerName] = useState(name);
 
   const handleEditClick = () => setIsEditing((prev) => !prev);
+  const handlePlayerNameChanged = (e) => setPlayerName(e.target.value);
 
-  let playerName = <span className="player-name">{name}</span>;
+  let playerName = <span className="player-name">{editablePlayerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" value={name} required />;
+    playerName = (
+      <input
+        type="text"
+        value={editablePlayerName}
+        onChange={handlePlayerNameChanged}
+        required
+      />
+    );
   }
 
   return (
