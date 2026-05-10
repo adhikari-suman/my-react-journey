62040b47d18a255426543c3d277fe28251222961
Suman Adhikari <expertxkid@gmail.com>
2026-05-09 22:42:14 -0500

    feat(04-react-essentials-deep-dive): add gameTurns state in App component and derive game board from it

--- 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  const handleSelectSquare = () => {
+  const handleSelectSquare = (rowIdx, colIdx) => {
     setActivePlayer((currentActivePlayer) =>
       currentActivePlayer == "X" ? "O" : "X",
     );
+
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIdx, col: colIdx }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   };
 
   return (
@@ -27,10 +44,8 @@ function App() {
           />
         </ol>
 
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <Log />
       </div>
     </main>
   );
