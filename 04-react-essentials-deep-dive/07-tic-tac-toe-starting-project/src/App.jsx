82e1fe9c9f5ea3c947fc6e2b16a8bb369abc9f81
Suman Adhikari <expertxkid@gmail.com>
2026-05-09 21:27:46 -0500

    feat(04-react-essentials-deep-dive): add static GameBoard component

--- 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
           <Player name={"Player 1"} symbol={"X"} />
           <Player name={"Player 2"} symbol={"O"} />
         </ol>
-        PLAYERS GAME BOARD
+
+        <GameBoard />
       </div>
     </main>
   );
