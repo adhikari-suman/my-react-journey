7ee542ad2bc3afdacffd67d3c3bf7d4cf49eac77
Suman Adhikari <expertxkid@gmail.com>
2026-05-09 20:29:10 -0500

    feat(04-react-essentials-deep-dive): add static Players section

--- 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ 04-react-essentials-deep-dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <p>Coming soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="plyaer-symbom">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="plyaer-symbom">O</span>
+          </li>
+        </ol>
+        PLAYERS GAME BOARD
+      </div>
+    </main>
+  );
 }
 
 export default App;
